f6e124db1d04011f296fd0cb49a95bc051cb120a
swifting.io <swifting.io@gmail.com>
2016-12-03 23:23:18 +0100

    Update ManagedObjectConvertible.swift

--- BetterCoreData/BetterCoreData/ManagedObjectConvertible.swift
+++ BetterCoreData/BetterCoreData/ManagedObjectConvertible.swift
@@ -1,64 +1,11 @@
 //
 //  ManagedObjectConvertible.swift
-//  BetterCoreData
-//
-//  Created by Michal Wojtysiak on 23/11/2016.
-//  Copyright © 2016 Michal Wojtysiak. All rights reserved.
+//  Created by swifting.io Team
 //
 
-import Foundation
 import CoreData
-protocol ManagedObjectProtocol {
-    associatedtype Entity
-    func toEntity() -> Entity?
-}
 
-extension ManagedObjectProtocol where Self: NSManagedObject {
-    
-    static func getOrCreateSingle(with id: String, from context: NSManagedObjectContext) -> Self {
-        let result = single(with: id, from: context) ?? insertNew(in: context)
-        result.setValue(id, forKey: "identifier")
-        return result
-    }
-    
-    static func single(from context: NSManagedObjectContext, with predicate: NSPredicate?,
-                       sortDescriptors: [NSSortDescriptor]?) -> Self? {
-        return fetch(from: context, with: predicate,
-                     sortDescriptors: sortDescriptors, fetchLimit: 1)?.first
-    }
-    
-    static func single(with id: String, from context: NSManagedObjectContext) -> Self? {
-        let predicate = NSPredicate(format: "identifier == %@", id)
-        return single(from: context, with: predicate, sortDescriptors: nil)
-    }
-    
-    static func insertNew(in context: NSManagedObjectContext) -> Self {
-        return Self(context:context)
-    }
-    
-    static func fetch(from context: NSManagedObjectContext, with predicate: NSPredicate?,
-                      sortDescriptors: [NSSortDescriptor]?, fetchLimit: Int?) -> [Self]? {
-        
-        let fetchRequest = Self.fetchRequest()
-        fetchRequest.sortDescriptors = sortDescriptors
-        fetchRequest.predicate = predicate
-        fetchRequest.returnsObjectsAsFaults = false
-        
-        if let fetchLimit = fetchLimit {
-            fetchRequest.fetchLimit = fetchLimit
-        }
-        
-        var result: [Self]?
-        context.performAndWait { () -> Void in
-            do {
-                result = try context.fetch(fetchRequest) as? [Self]
-            } catch {
-                result = nil
-                //Report Error
-                print("CoreData fetch error \(error)")
-            }
-        }
-        return result
-    }
-
+protocol ManagedObjectConvertible {
+    associatedtype ManagedObject: NSManagedObject, ManagedObjectProtocol
+    func toManagedObject(in context: NSManagedObjectContext) -> ManagedObject?
 }
